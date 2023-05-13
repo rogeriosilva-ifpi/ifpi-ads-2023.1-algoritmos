3b1e658ea2fe8fe47cf70f1e7050bbec7967e419
Rogério Silva <rogerio410@gmail.com>
2023-05-13 10:40:41 -0300

    Labs

---
+++ r1_labs/bee/multiple_values_smart.py
@@ -0,0 +1,9 @@
+def main():
+    #  Input: "5 61 -70 8"
+    n1, n2, n3, n4 = map(int, input().split())
+
+    print(f'v1: {n1}, v2: {n2}, v2: {n3}, v4: {n4}')
+    # Output: "v1: 5, v2: 61, v2: -70, v4: 8"
+
+
+main()
